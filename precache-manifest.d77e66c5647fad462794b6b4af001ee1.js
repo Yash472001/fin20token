720f1e4a5540f2aa49fa71778009fcf2b6c0b151
Yash472001 <yashp.4721@gmail.com>
2021-08-26 10:10:30 +0530

    Updates

---
+++ precache-manifest.d77e66c5647fad462794b6b4af001ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78bb3633954e118e55494a629847799",
+    "url": "/fin20token/index.html"
+  },
+  {
+    "revision": "91d982a619cc51c0f027",
+    "url": "/fin20token/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e6bd2f11a2a37dc8309e",
+    "url": "/fin20token/static/js/2.045e15fb.chunk.js"
+  },
+  {
+    "revision": "91d982a619cc51c0f027",
+    "url": "/fin20token/static/js/main.0743cef4.chunk.js"
+  },
+  {
+    "revision": "f55c1ceed35a60bf61d3",
+    "url": "/fin20token/static/js/runtime-main.33142f22.js"
+  }
+]);
